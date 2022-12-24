2c9c55ede48fec11718368cb9d10e19078f8ca69
rayyze93 <rayan-btk@hotmail.com>
2022-12-24 13:44:35 -0800

    cub3D

--- src/cub3D.c
+++ src/cub3D.c
@@ -22,7 +22,7 @@ static int	parsing(int ac, char **av, t_data *data, int len)
 	len = ft_strlen(av[1]);
 	if (len < 4 || av[1][len - 1] != 'b' || av[1][len - 2] != 'u'
 		|| av[1][len - 3] != 'c' || av[1][len - 4] != '.')
-		return (write(2, "Error\nFile is not a ->cub\n", 26), -1);
+		return (write(2, "Error\nFile is not a .cub\n", 25), -1);
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 		return (write(2, "Error\n", 6), perror(av[1]), -1);
